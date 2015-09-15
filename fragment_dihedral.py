fbb343a0b29a8b6f7eea22b338f24fc06348d19b
Bertrand Caron <bertrand.fx.caron@gmail.com>
2015-09-16 07:28:01 +1000

    Rewrite the dihedral objects: the interesting neighbours are 2 and 3, not 1 and 4

--- fragment_dihedral.py
+++ fragment_dihedral.py
@@ -4,15 +4,22 @@ class FragmentDihedral(object):
     def __init__(self, dihedral_string):
         splitted_string = dihedral_string.split("|")
 
-        self.atom1 = splitted_string[0][0]
-        self.neighbours1 = list(splitted_string[0][1:])
-        self.atom2 = splitted_string[1][0]
-        self.atom3 = splitted_string[1][1]
-        self.atom4 = splitted_string[2][0]
-        self.neighbours4 = list(splitted_string[2][1:])
+        self.atom_1 = splitted_string[0][0]
+        self.atom_2 = splitted_string[1][0]
+        self.neighbours_2 = list(splitted_string[1].split('-')[1].split(','))
+        self.atom_3 = splitted_string[2][0]
+        self.neighbours_3 = list(splitted_string[2].split('-')[1].split(','))
+        self.atom_4 = splitted_string[3][0]
 
     def __str__(self):
-        return "{atom1}{neighbours1}|{atom2}{atom3}|{atom4}{neighbours4}".format(atom1=self.atom1, atom2=self.atom2, atom3=self.atom3, atom4=self.atom4, neighbours1="".join(self.neighbours1), neighbours4="".join(self.neighbours4))
+        return "{atom_1}|{atom_2}-{neighbours_2}|{atom_3}-{neighbours_3}|{atom_4}".format(
+            atom_1=self.atom_1,
+            atom_2=self.atom_2,
+            neighbours_2=",".join(self.neighbours_2),
+            atom_3=self.atom_3,
+            neighbours_3=",".join(self.neighbours_3),
+            atom_4=self.atom_4,
+        )
 
     def __eq__(self, other):
         return self.__canonical_rep__().__dict__ == other.__canonical_rep__().__dict__
@@ -24,17 +31,20 @@ class FragmentDihedral(object):
         other = deepcopy(self)
         # Order each neighbour list by alphabetical order
         # WARNING: Keep in mind that maintaining order will be necessary for maintaining sterochemistry information
-        other.neighbours1.sort()
-        other.neighbours4.sort()
+        other.neighbours_2.sort()
+        other.neighbours_3.sort()
         # Compare the two neighbour list and put the first in lexical order on the leftmost side
-        if [ other.atom4 ] + other.neighbours4 <= [ other.atom1 ] + other.neighbours1:
-            other.atom1, other.atom4 = other.atom4, other.atom1
-            other.neighbours1, other.neighbours4 = other.neighbours4, other.neighbours1
+        if [other.atom_3] + other.neighbours_3 <=  [other.atom_2] + other.neighbours_2:
+            other.atom_1, other.atom_4 = other.atom_4, other.atom_1
+            other.neighbours_2, other.neighbours3 = other.neighbours_3, other.neighbours_2
         return other
 
 if __name__ == "__main__" :
-    dihedral = FragmentDihedral("CAB|CC|CHH")
+    dihedral = FragmentDihedral("C|C-C,H|C-H,H|C")
     print dihedral
-    dihedral2 = FragmentDihedral("CHH|CC|CAB")
-    print dihedral2.__canonical_rep__()
-    print dihedral == dihedral2
+    dihedral_2 = FragmentDihedral("C|C-H,C|C-H,H|C")
+    print dihedral_2.__canonical_rep__()
+    print dihedral == dihedral_2
+    dihedral_3 = FragmentDihedral("C|C-H,C|C-H,C|C")
+    print dihedral_3 == dihedral_2
+
