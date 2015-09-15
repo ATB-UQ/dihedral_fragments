c7ae5b001524d784856950425e385b441cfc536d
Bertrand Caron <bertrand.fx.caron@gmail.com>
2015-09-16 07:33:06 +1000

    Update init method to generate dihedrals from a list

--- fragment_dihedral.py
+++ fragment_dihedral.py
@@ -1,15 +1,18 @@
 from copy import deepcopy
 class FragmentDihedral(object):
 
-    def __init__(self, dihedral_string):
-        splitted_string = dihedral_string.split("|")
+    def __init__(self, dihedral_string=None, atom_list=None):
 
-        self.atom_1 = splitted_string[0][0]
-        self.atom_2 = splitted_string[1][0]
-        self.neighbours_2 = list(splitted_string[1].split('-')[1].split(','))
-        self.atom_3 = splitted_string[2][0]
-        self.neighbours_3 = list(splitted_string[2].split('-')[1].split(','))
-        self.atom_4 = splitted_string[3][0]
+        if dihedral_string:
+            splitted_string = dihedral_string.split("|")
+            self.atom_1 = splitted_string[0][0]
+            self.atom_2 = splitted_string[1][0]
+            self.neighbours_2 = list(splitted_string[1].split('-')[1].split(','))
+            self.atom_3 = splitted_string[2][0]
+            self.neighbours_3 = list(splitted_string[2].split('-')[1].split(','))
+            self.atom_4 = splitted_string[3][0]
+        else:
+            self.atom_1, self.atom_2, self.neighbours_2, self.atom_3, self.neighbours_3, self.atom_4 = atom_list
 
     def __str__(self):
         return "{atom_1}|{atom_2}-{neighbours_2}|{atom_3}-{neighbours_3}|{atom_4}".format(
@@ -47,4 +50,6 @@ if __name__ == "__main__" :
     print dihedral == dihedral_2
     dihedral_3 = FragmentDihedral("C|C-H,C|C-H,C|C")
     print dihedral_3 == dihedral_2
+    dihedral_4 = FragmentDihedral(atom_list=('C', 'C', ['H', 'H'], 'C', ['Cl', 'Cl'], 'B'))
+    print dihedral_4
 
