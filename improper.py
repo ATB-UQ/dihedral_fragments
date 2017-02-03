6ea110d2a2b3f8542dce592b41747a2d938f1d53
Bertrand Caron <bertrand.fx.caron@gmail.com>
2017-02-03 15:56:36 +1000

    Feature: Mock up an Improper class

---
+++ improper.py
@@ -0,0 +1,24 @@
+from typing import List, Any, Optional
+
+from fragment_dihedrals.fragment_dihedral import GROUP_SEPARATOR, on_asc_number_electron_then_asc_valence, NEIGHBOUR_SEPARATOR
+
+class Improper(object):
+    def __init__(self, central: Optional[str] = None, neighbours: Optional[List[str]] = None, improper_str: Optional[str] = None) -> None:
+        if improper_str is not None:
+            fields = improper_str.split(GROUP_SEPARATOR)
+            assert len(fields) == 2, fields
+            self.central, self.neighbours = fields[0], fields[1].split(NEIGHBOUR_SEPARATOR)
+        else:
+            self.central, self.neighbours = central, neighbours
+
+        self.neighbours = sorted(
+            self.neighbours,
+            key=on_asc_number_electron_then_asc_valence,
+            reverse=True,
+        )
+
+    def __str__(self) -> str:
+        return 'Improper(central={0}, neighbours={1})'.format(self.central, self.neighbours)
+
+if __name__ == '__main__':
+    print(Improper(improper_str='C|C,C,H'))
